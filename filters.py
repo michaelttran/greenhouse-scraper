6bace54260ea58e267f446a81b0e302ce43d20c6
Michael Tran <15336230+michaelttran@users.noreply.github.com>
2025-07-30 13:32:05 -0700

    Adds another webscraper to find partners through gh

--- filters.py
+++ filters.py
@@ -1,9 +1,44 @@
 companies = [
-    "attain", # single page
-    "reddit",
-    "twilio", # paginated
-    "checkr"
+    'appcast',
+    'attain',
+    'bob',
+    'brighthire',
+    'cake',
+    'calendly',
+    'checkr',
+    'clara',
+    'coda',
+    'covey',
+    'dropbox',
+    'endorsed',
+    'erin',
+    'ethyca',
+    'fivetran',
+    'glassdoor',
+    'gusto',
+    'handshake',
+    'hyreo',
+    'indeed',
+    'karat',
+    'leap',
+    'linkedin',
+    'merge',
+    'naukri',
+    'offerup',
+    'okta',
+    'onetrust',
+    'pandadoc',
+    'recruitics',
+    'reddit',
+    'remote',
+    'sterling',
+    'textio',
+    'twilio',
+    'woo',
+    'workato',
+    'yext',
 ]
+
 allowlist_job_titles = [
     "senior software engineer",
     "software engineer"
--- find_greenhouse_partners.py
+++ find_greenhouse_partners.py
@@ -1,20 +1,33 @@
+# threading
 """
 Benchmarks:
-    - Pre-threading: 51.41468405723572s
+    - Pre-threading: 51.41468405723572s || 529 partners
+    - Post-threading: 5.155369997024536s || 529 partners
 """
 from bs4 import BeautifulSoup
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import ThreadPoolExecutor
 import re
 import requests
 import random
 import time
 
-from constants import PARTNERS_URL
+from constants import BASE_URL, PARTNERS_URL
+
+ALL_INTEGRATIONS = []
+
+INTEGRATIONS_WITH_JOB_PAGE = []
+
+HEADERS = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/115.0.0.0 Safari/537.36"
+    )
+}
 
 def get_last_page_number() -> int:
     response = requests.get(f"{PARTNERS_URL}1")
     soup = BeautifulSoup(response.text, "html.parser")
-    # HTML code: line number 13391
     lis = soup.find("li", class_="last next page-item").select_one("a")
     page_num = lis["href"]
     match = re.search(r"page=(\d+)", page_num)
@@ -25,37 +38,49 @@ def get_last_page_number() -> int:
         raise Exception(f"Couldn't find last page number with {PARTNERS_URL}")
 
 def scrape_greenhouse_partners(url):
-    pass
+    response = requests.get(url)
+    time.sleep(random.uniform(0, 1)) # TODO: Add this so their lb won't ban IP
+    soup = BeautifulSoup(response.text, "html.parser")
+    classes = soup.find_all(class_="pf-card card-grid__card card-grid__card-flat card-style-shadow")
+    for c in classes:
+        href = c.get("href")
+        if href is None:
+            continue
+        match = re.search(r'^/[^/]+/(.+)$', href)
+        if match:
+            name = match.group(1)
+            ALL_INTEGRATIONS.append(name)
+        else:
+            print(f"no match: href -> {href}")    
 
 def get_partners(total_pages):
-    all_integrations = []
-    for page in range(1, total_pages + 1):
-        url = f"{PARTNERS_URL}{page}"
-        response = requests.get(url)
-        # time.sleep(random.uniform(0, 1)) # TODO: Add this so their lb won't ban IP
-        soup = BeautifulSoup(response.text, "html.parser")
-        classes = soup.find_all(class_="pf-card card-grid__card card-grid__card-flat card-style-shadow")
-        for c in classes:
-            href = c.get("href")
-            if href is None:
-                continue
-            match = re.search(r'^/[^/]+/(.+)$', href)
-            if match:
-                name = match.group(1)
-                print(f"matched: {name}")
-                all_integrations.append(name)
-            else:
-                print(f"no match: href -> {href}")
-    return all_integrations
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        futures = [executor.submit(scrape_greenhouse_partners, f"{PARTNERS_URL}{page}") for page in range(1, total_pages + 1)]
+
+def check_partner_job_page_200(url, partner):
+    response = requests.get(url, headers=HEADERS) # Some companies require headers
+    if response.status_code == 200:
+        INTEGRATIONS_WITH_JOB_PAGE.append(partner)
+    if response.status_code != 200 and response.status_code != 404:
+        print(f"{response.status_code} -- {partner}: {url} || {response.reason}")
+
+def get_partner_job_page(BASE_URL):
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        futures = [executor.submit(check_partner_job_page_200, f"{BASE_URL}{partner}", partner) for partner in ALL_INTEGRATIONS]
 
 def main():
     total_pages = get_last_page_number()
     print(f"total_pages: {total_pages}")
+    
+    get_partners(total_pages)
+
     start = time.time()
-    partners = get_partners(total_pages)
+    get_partner_job_page(BASE_URL)
     end = time.time()
-    print(f"partners: {partners}")
     print(f"total time: {end-start}")
+    INTEGRATIONS_WITH_JOB_PAGE.sort()
+    print(f"INTEGRATIONS_WITH_JOB_PAGE: {INTEGRATIONS_WITH_JOB_PAGE}")
+    print(f"Found {len(INTEGRATIONS_WITH_JOB_PAGE)} partners' job pages")
 
 if __name__ == "__main__":
     main()
